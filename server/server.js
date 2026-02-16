2cd0590d988a32649fd1604f50012790927e09ed
ddushar <dushardanya3@gmail.com>
2026-02-16 21:12:30 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,11 +6,13 @@ const serverRoutes = require("./routes/servers");
 const donateRoutes = require("./routes/donate");
 
 const app = express();
-app.use(cors());
+app.use(cors()); // разрешить запросы с любого источника
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/servers", serverRoutes);
 app.use("/api/donate", donateRoutes);
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+// ⚡ использовать порт от Render или 5000 по умолчанию
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => co
