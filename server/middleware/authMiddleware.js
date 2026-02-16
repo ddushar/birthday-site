c98b040f49c27ade4f6ced66b30d1c3ff9386373
ddushar <dushardanya3@gmail.com>
2026-02-16 20:46:37 +0100

    Create authMiddleware.js

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], "kilop_secret");
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+module.exports = auth;
