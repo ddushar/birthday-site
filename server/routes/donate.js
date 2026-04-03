846e01669b45532050be0c458b299379aeeb25ee
ddushar <dushardanya3@gmail.com>
2026-04-03 11:50:53 +0200

    Delete server directory

--- server/routes/donate.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res) => {
-  const { package } = req.body;
-  res.json({ message: `Оплата пакета ${package} создана` });
-});
-
-module.exports = router;
